2816270901c2b15b4dd3d7494f700b35dbf16793
daviduscategui <uscategui.dav07@gmail.com>
2026-05-22 19:53:42 -0500

    main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from grafo import grafo_A, grafo_B
 from mis import ejecutar as mis
 from minimax import ejecutar as minimax
 
-print("=== Escenario A ===")
+print("Escenario A")
 mis(grafo_A(), "A (10 nodos)")
 minimax(grafo_A(), "N0", "N9", "A (10 nodos)")
 
-print("\n=== Escenario B ===")
+print("Escenario B")
 mis(grafo_B(), "B (denso)")
 minimax(grafo_B(), "N0", "N5", "B (denso)")
